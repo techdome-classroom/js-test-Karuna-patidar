86d233039a8fbaee962442ac732674a63af59c17
karuna <karunapatidar02062gmail.com>
2024-07-26 13:02:41 +0530

    hello

--- program1.js
+++ program1.js
@@ -1,8 +1,25 @@
 const getTotalIsles = function (grid) {
-
-
-  // write your code here
-
-};
-
-module.exports = getTotalIsles;
+  const row = grid.length;
+  const col = grid[0].length;
+  let isl = 0;
+  const flag = (i, j) => {
+      if (i < 0 || i >= row || j < 0 || j >= col || grid[i][j] !== 'L') {
+          return;
+      }
+      grid[i][j] = '#'; 
+      flag(i + 1, j);
+      flag(i - 1, j);
+      flag(i, j + 1);
+      flag(i, j - 1);
+  };
+  for (let i = 0; i < row; i++) {
+      for (let j = 0; j < col; j++) {
+          if (grid[i][j] === 'L') {
+              flag(i, j);
+              isl++;
+          }
+      }
+  }
+  return isl;
+  };
+  module.exports = getTotalIsles;
